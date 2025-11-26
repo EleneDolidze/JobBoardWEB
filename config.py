ce3c3021d134da0f6cffeb74c03963c7429ca97d
Elene Dolidze <eleneedolidze@gmail.com>
2025-11-27 00:50:41 +0400

    changed comfig.py

--- config.py
+++ config.py
@@ -2,11 +2,13 @@
 #მონაცემთა ბაზის მისამართის გაწერა
 
 import os
+
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = "mysecretkey"
-
-    #ეს ქმნის SQLITE მონაცემთა ბაზას - jobboard.db დირექტორიაში
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'jobboard.db')}"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "mysecretkey")
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        f"sqlite:///{os.path.join(BASE_DIR, 'jobboard.db')}"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
